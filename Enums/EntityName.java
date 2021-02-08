ce987220c98cc2470c4f5c92ea7a41c6e2274141
Eduardo Mior <eduardo-mior@hotmail.com>
2021-02-08 12:52:00 -0300

    Update EntityName.java

--- Enums/EntityName.java
+++ Enums/EntityName.java
@@ -70,7 +70,7 @@ public enum EntityName {
 	PHANTOM("Phantom"),
 	PIG("Porco"),
 	PIGLIN("Piglin"),
-	PIGLIN("Piglin Bárbaro"),
+	PIGLIN_BRUTE("Piglin Bárbaro"),
 	PIG_ZOMBIE("Porco Zumbi"),
 	PILLAGER("Saqueador"),
 	PLAYER("Player"),
